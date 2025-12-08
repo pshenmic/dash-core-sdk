b7e9971337c6718fe9bce643507ada400a9ba694
owl352 <kokosinca123@gmail.com>
2025-12-09 01:13:26 +0300

    block hash

--- src/types/BlockHeader.ts
+++ src/types/BlockHeader.ts
@@ -1,5 +1,6 @@
 import { bytesToHex, hexToBytes } from '../utils'
 import { BlockHeaderJSON } from '../types'
+import x11 from '@dashevo/x11-hash-js'
 
 export class BlockHeader {
   version: number
@@ -16,6 +17,37 @@ export class BlockHeader {
     this.time = time
     this.nBits = nBits
     this.nonce = nonce
+  }
+
+  hash (): string {
+    return bytesToHex(new Uint8Array(x11.digest([...this.bytes()], 1, 1).toReversed()))
+  }
+
+  bytes (): Uint8Array {
+    const versionDataView = new DataView(new ArrayBuffer(4))
+    const timeDataView = new DataView(new ArrayBuffer(4))
+    const nBitsDataView = new DataView(new ArrayBuffer(4))
+    const nonceDataView = new DataView(new ArrayBuffer(4))
+
+    versionDataView.setUint32(0, this.version, true)
+    timeDataView.setUint32(0, this.time, true)
+    nBitsDataView.setUint32(0, this.nBits, true)
+    nonceDataView.setUint32(0, this.nonce, true)
+
+    const out = new Uint8Array(80)
+
+    out.set(new Uint8Array(versionDataView.buffer), 0)
+    out.set(this.previousBlockHash.toReversed(), 4)
+    out.set(this.merkleRoot.toReversed(), 36)
+    out.set(new Uint8Array(timeDataView.buffer), 68)
+    out.set(new Uint8Array(nBitsDataView.buffer), 72)
+    out.set(new Uint8Array(nonceDataView.buffer), 76)
+
+    return out
+  }
+
+  hex (): string {
+    return bytesToHex(this.bytes())
   }
 
   static fromBytes (bytes: Uint8Array): BlockHeader {
