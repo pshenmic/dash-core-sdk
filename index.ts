eaa71776140588d8a89af941132bdf560e2e84fa
pshenmic <pshenmic@gmail.com>
2026-03-02 12:21:57 +0700

    Export types

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-import { DashCoreSDK } from './src/DashCoreSDK.js'
+import {DashCoreSDK} from './src/DashCoreSDK.js'
+import {BlockHeader} from "./src/types/BlockHeader.js"
+import {BloomFilterWriter} from "./src/types/BloomFilter.js"
+import {Input} from "./src/types/Input.js"
+import {InstantLock} from "./src/types/InstantLock.js"
+import {MerkleBlock} from "./src/types/MerkleBlock.js"
+import {MerkleTree} from "./src/types/MerkleTree.js"
+import {OutPoint} from "./src/types/OutPoint.js"
+import {Output} from "./src/types/Output.js"
+import {PrivateKey} from "./src/types/PrivateKey.js"
+import {PublicKey} from "./src/types/PublicKey.js"
+import {Script} from "./src/types/Script.js"
+import {Transaction} from './src/types/Transaction.js'
 
-export { DashCoreSDK }
+export {
+    DashCoreSDK,
+    BlockHeader,
+    BloomFilterWriter,
+    Input,
+    InstantLock,
+    MerkleBlock,
+    MerkleTree,
+    OutPoint,
+    Output,
+    PrivateKey,
+    PublicKey,
+    Script,
+    Transaction
+}
