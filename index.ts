4a90c42b0215055ea36cbb42a53e0fd5c81c532a
LexxXell <lexxxell007@gmail.com>
2026-03-30 14:43:38 +0400

    fix: exports

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
-import { DashCoreSDK } from './src/DashCoreSDK.js'
+export {
+  BlockHeader,
+  BloomFilterWriter,
+  DashCoreSDK,
+  ExtraPayload,
+  Input,
+  InstantLock,
+  MerkleBlock,
+  MerkleTree,
+  Messages,
+  OutPoint,
+  Output,
+  PrivateKey,
+  PublicKey,
+  Script,
+  Transaction
+} from './src/DashCoreSDK.js'
 
-export { DashCoreSDK }
+export type {
+  BloomFilter,
+  CoreKeyPair,
+  PaymentInfo,
+  SubscribeToTransactionsEvent
+} from './src/DashCoreSDK.js'
